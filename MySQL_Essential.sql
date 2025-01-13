130bf38025ad899af21689b649546a9ea203b751
monktar <monktar@gmail.com>
2025-01-13 14:09:42 -0600

    Create MySQL_Essential.sql

---
+++ MySQL_Essential.sql
@@ -0,0 +1,18 @@
+MySQL Essential
+
+
+-- Long Running Queries with PROCESSLIST
+
+SELECT 
+    id AS pid,
+    user AS usename,
+    TIME_TO_SEC(TIMEDIFF(NOW(), start_time)) AS duration, -- Duration in seconds
+    info AS query
+FROM 
+    INFORMATION_SCHEMA.PROCESSLIST
+WHERE 
+    info IS NOT NULL
+    AND TIME_TO_SEC(TIMEDIFF(NOW(), start_time)) > 30 -- Queries running longer than 30 seconds
+    AND user NOT IN ('claimresearch', 'enterprisedb')
+ORDER BY 
+    TIME_TO_SEC(TIMEDIFF(NOW(), start_time)) DESC;
